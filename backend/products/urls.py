ce8308a7f9e8e6f0222455c2ba9ad3473308034f
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-28 00:02:08 +0100

    create_view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import product_detail_view
+from .views import product_detail_view, product_create_view
 
 urlpatterns = [
+    path("", product_create_view ),
     path("<int:pk>/" , product_detail_view)
 ]
