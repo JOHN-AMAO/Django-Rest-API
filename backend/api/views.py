d60f39402095b292ac5b975ce68e9fa51163a032
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-27 20:42:40 +0100

    .

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,13 +4,9 @@ from products.models import Product
 from django.forms.models import model_to_dict
 from products.serializers import ProductSerializers
 
-@api_view(["GET"])
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        data = ProductSerializers(instance).data
-        #data = model_to_dict(model_data, fields=["id", "title", "content", "sale_price"])
+    data = request.data
     return Response(data)
 
 
--- py_client/pyclient.py
+++ py_client/pyclient.py
@@ -3,6 +3,6 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+get_response = requests.post(endpoint, json={"name": "John"})
 
 print(get_response.text)
