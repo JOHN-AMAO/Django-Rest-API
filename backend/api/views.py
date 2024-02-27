0143b088a5ff5a9917cc947a36ff3840b67cf26c
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-27 19:15:44 +0100

    working api with serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,15 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from products.models import Product
 from django.forms.models import model_to_dict
+from products.serializers import ProductSerializers
 
 @api_view(["GET"])
-def api_home(request):
-    model_data = Product.objects.all().order_by("?").first()
+def api_home(request, *args, **kwargs):
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    if model_data:
-        data = model_to_dict(model_data, fields=["id", "title", "content", "sale_price"])
+    if instance:
+        data = ProductSerializers(instance).data
+        #data = model_to_dict(model_data, fields=["id", "title", "content", "sale_price"])
     return Response(data)
 
 
