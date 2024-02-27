f931c904602f100babe13837fdf76730c84790c2
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-27 23:49:06 +0100

    workingapi__generics

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import product_detail_view
 
 urlpatterns = [
-    path("<int:pk>" , views.ProductDetailAPIView.as_view)
+    path("<int:pk>/" , product_detail_view)
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -9,3 +9,5 @@ from .serializers import ProductSerializers
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
+
+product_detail_view = ProductDetailAPIView.as_view()
