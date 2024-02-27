94bbc545d109c6de96832e56fc5abf0d753ca964
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-27 18:52:45 +0100

    api working ad returning response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,14 @@
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from products.models import Product
+from django.forms.models import model_to_dict
 
-@api_view(["GET", "POST"])
+@api_view(["GET"])
 def api_home(request):
-    response = request.body
-    return Response(response)
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=["id", "title", "content", "sale_price"])
+    return Response(data)
 
 
