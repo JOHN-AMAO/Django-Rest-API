64cc647a9bf108fb5e006017f3f03d924bdad021
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-28 22:57:39 +0100

    .

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,6 +5,6 @@ from .views import product_detail_view,product_update_view, product_list_create_
 urlpatterns = [
     path("", product_list_create_view ),
     path("<int:pk>/update" , product_update_view),
-        path("<int:pk>/delete" , product_delete_view),
+    path("<int:pk>/delete" , product_delete_view),
     path("<int:pk>/" , product_detail_view)
 ]
