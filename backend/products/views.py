0a18e408f85b9c2a1071bae5c67be21c5cb58199
JOHN-AMAO <amaojohn767@gmail.com>
2024-02-29 23:50:34 +0100

    mixins and shit

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,35 +1,35 @@
 from rest_framework import generics,permissions, authentication
 # Create your views here.
 
-
+from api.mixins import StaffEditorPermissionMixin 
 from .models import Product
 from .serializers import ProductSerializers
 
 
-class ProductListCreateAPIView(generics.ListCreateAPIView):
+class ProductListCreateAPIView(StaffEditorPermissionMixin, generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     authentication_classes = [authentication.SessionAuthentication, 
                               authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
+   
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
+class ProductDetailAPIView(StaffEditorPermissionMixin,generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     #lookup_field = "pk"
 
 product_detail_view = ProductDetailAPIView.as_view()
 
-class ProductUpdateAPIView(generics.UpdateAPIView):
+class ProductUpdateAPIView(StaffEditorPermissionMixin,generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     lookup_field = "pk"
 
 product_update_view = ProductUpdateAPIView.as_view()
 
-class ProductDeleteAPIView(generics.DestroyAPIView):
+class ProductDeleteAPIView(StaffEditorPermissionMixin,generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     lookup_field = "pk"
